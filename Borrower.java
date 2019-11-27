f9c66ca4e7b3fb2f71b24a36d2f37b55a94eae6f
shindongjun <cry4563@naver.com>
2019-11-27 17:51:43 +0900

    대출자 클래스 작성

--- Borrower.java
+++ Borrower.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * 여기에 Borrower 클래스 설명을 작성하십시오.
  * 
@@ -7,27 +7,24 @@
  */
 public class Borrower
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * Borrower 클래스의 객체 생성자
-     */
-    public Borrower()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
+    private String name;
+    public HashSet<Borrower> br;
+    public void Borrower(String name){
+        
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public void attachBook(){
+    
+    }
+    public void detachBook(){
+    
+    }
+    public int CompareTo(Object obj){
+        return 0;
+    }
+    public boolean equals(Object obj){
+        return obj;
+    }
+    public int hashCode(){
+        return 0;
     }
 }
