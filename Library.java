3261ad86ab2a9abed8221c2f075b942a3c4a039c
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-11-27 17:55:44 +0900

    Library class 메소드 생성

--- Library.java
+++ Library.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * 여기에 Library 클래스 설명을 작성하십시오.
  * 
@@ -7,27 +7,35 @@
  */
 public class Library
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * Library 클래스의 객체 생성자
-     */
-    public Library()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
+    private String name;
+    private TreeSet<Book> registeredBooks;
+    private HashSet<Borrower> registeredBorrower;
+    
+    public Library(String name){
+        
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    
+    public void registerOneBorrower(String name){
+        
+    }
+    
+    public void registerOneBook(String title, String author, int catalogueNumber){
+        
+    }
+    
+    public void displayBooksForLoan(){
+        
+    }
+    
+    public void displayBooksOnLoan(){
+        
+    }
+    
+    public void LendOneBook(String name, int catalogueNumber){
+        
+    }
+    
+    public void returnOneBook(int catalogueNumber){
+        
     }
 }
