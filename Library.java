14361851dad6ec28a71b6f148f17b7df6ac78600
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-11-28 19:21:53 +0900

    Library 클래스 시퀀스 1,2,3,4 작성

--- Library.java
+++ Library.java
@@ -11,25 +11,47 @@ public class Library
     private TreeSet<Book> registeredBooks;
     private HashSet<Borrower> registeredBorrower;
     
-    public Library(String name){
-        
+   public Library(String name){
+        this.name = name;
+        registeredBooks = new TreeSet<Book>();
+        registeredBorrowers = new HashSet<Borrower>();
     }
-    
+
     public void registerOneBorrower(String name){
-        
+        Borrower borrower = new Borrower(name);
+        registeredBorrowers.add(borrower);
     }
-    
+
     public void registerOneBook(String title, String author, int catalogueNumber){
-        
+        Book book = new Book(title, author, catalogueNumber);
+        registeredBooks.add(book);
     }
+
+    //Loan객체와 관련이 있는지 물어보기
+    //관련있으면 보여주기 없으면 안보여주기
     
     public void displayBooksForLoan(){
-        
+        Iterator it = registeredBooks.iterator();
+        while(it.hasNext()){
+            if((book.getBorrower())==null){
+                
+            }
+            else{
+                book.display();
+            }
+        }
     }
     
+    //대출 가능한 책
     public void displayBooksOnLoan(){
-        
-    }
+        Iterator it = registeredBooks.iterator();
+        while(it.hasNext()){
+            Book book = (Book)it.next();
+            if((book.getBorrower())==null){
+                book.display();
+            }
+        }
+    } 
     
     public void LendOneBook(String name, int catalogueNumber){
         
