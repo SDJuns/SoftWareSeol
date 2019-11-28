a4a88f06a1b931105b4b7258672d6b6d0a5103b7
PARKSUNGHYUN1 <smecpat@nate.com>
2019-11-28 21:01:36 +0900

    dd

--- Library.java
+++ Library.java
@@ -1,17 +1,17 @@
 import java.util.*;
 /**
- * 여기에 Library 클래스 설명을 작성하십시오.
+ * 여기에 ClassIS 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
+ * @author (2) 
  * @version (버전번호나 날짜)
  */
 public class Library
 {
     private String name;
     private TreeSet<Book> registeredBooks;
-    private HashSet<Borrower> registeredBorrower;
-    
-   public Library(String name){
+    private HashSet<Borrower> registeredBorrowers;
+
+    public Library(String name){
         this.name = name;
         registeredBooks = new TreeSet<Book>();
         registeredBorrowers = new HashSet<Borrower>();
@@ -29,21 +29,6 @@ public class Library
 
     //Loan객체와 관련이 있는지 물어보기
     //관련있으면 보여주기 없으면 안보여주기
-    
-    public void displayBooksForLoan(){
-        Iterator it = registeredBooks.iterator();
-        while(it.hasNext()){
-            Book book = (Book)it.next();
-            if((book.getBorrower())==null){
-                book.display();
-            }
-            else{
-                book.display();
-            }
-        }
-    }
-    
-    //대출 가능한 책
     public void displayBooksOnLoan(){
         Iterator it = registeredBooks.iterator();
         while(it.hasNext()){
@@ -55,16 +40,27 @@ public class Library
                 book.display();
             }
         }
-    } 
+    }
     
-    public void LendOneBook(String name, int catalogueNumber){
+    public void displayBooksForLoan(){
+        Iterator it = registeredBooks.iterator();
+        while(it.hasNext()){
+            Book book = (Book)it.next();1
+            if((book.getBorrower())==null){
+                book.display();
+            }
+        }
+    }
+    
+    
+
+    public void LeadOneBook(String name, int catalogueNumber){
         Book book = this.findBook(catalogueNumber);
         Borrower borrower = this.findBorrower(name);
-        Loan loan = new Loan(book, borrower);
+        
         if(book!=null && borrower!=null){
             book.attachBorrower(borrower);
             borrower.attachBook(book);
-            loan.returndate();
         }
         else if(book==null){
             System.out.println("책을 찾을 수 없음");
@@ -75,9 +71,18 @@ public class Library
     }
 
     public void returnOneBook(int catalogueNumber){
-        Book book = this.findBook(catalogueNumber);
-        Loan loan = this.findLoan(book);
-        loan.claer();
-        
+        Book result = null;
+        Iterator it = registeredBooks.iterator();
+        while(it.hasNext()){
+            Book book = (Book)it.next();
+            int bookcatalogueNumber =book.getcatalogueNumber();
+            if(bookcatalogueNumber==catalogueNumber){
+                
+            }
+
+        }
     }
+    
 }
+
+    
