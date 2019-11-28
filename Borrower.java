b4631072a0c067270e780ef725f74a0b47e6ced6
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-11-28 21:34:20 +0900

    날짜 기입

--- Borrower.java
+++ Borrower.java
@@ -3,14 +3,13 @@ import java.util.*;
  * 여기에 Borrower 클래스 설명을 작성하십시오.
  * 
  * @author (2018315009 황훈태, 2018315025 신동준, 2018315031 박성현) 
- * @version (버전번호나 날짜)
+ * @version (2019-11-28)
  */
 public class Borrower
 {
     private String name;
     private LinkedList<Book> borrowedBooks;
-    private Borrower borrower;
-    private Book book;
+   
 
     public Borrower(String name){
         this.name = name;
--- Library.java
+++ Library.java
@@ -3,7 +3,7 @@ import java.util.*;
  * 여기에 ClassIS 클래스 설명을 작성하십시오.
  * 
  * @author (2018315009 황훈태, 2018315025 신동준, 2018315031 박성현) 
- * @version (버전번호나 날짜)
+ * @version (2019-11-28)
  */
 public class Library
 {
